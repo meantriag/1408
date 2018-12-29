05276900ced3ae1166b240f9f42c404f8f31d11f
Mean Triag <triangleweakapuz@gmail.com>
2018-12-29 11:11:33 +0000

    1408

--- js/game_manager.js
+++ js/game_manager.js
@@ -4,7 +4,7 @@ function GameManager(size, InputManager, Actuator, ScoreManager) {
   this.scoreManager = new ScoreManager;
   this.actuator     = new Actuator;
 
-  this.startTiles   = 2;
+  this.startTiles   = 1;
 
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.75 ? 2 : Math.random() < 0.5 ? 4 : 0;
+    var value = Math.random() < 0.9 ? 128 : Math.random() < 0.99 ? 256 : 384 ;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -128,7 +128,7 @@ GameManager.prototype.move = function (direction) {
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom || next && next.value == 0 || next && tile.value == 0) {
           
-	        var merged = new Tile(positions.next, tile.value * 2);
+	        var merged = new Tile(positions.next, tile.value + 128);
           if(next && next.value == 0) merged.value = tile.value / 2;
           if(next && tile.value == 0) merged.value = next.value / 2;
           merged.mergedFrom = [tile, next];
@@ -142,8 +142,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 1408 tile
+          if (merged.value === 1408) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
